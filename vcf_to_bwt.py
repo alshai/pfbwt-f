6846a2b28b285292b39479d59c2544c2f6d2c463
Taher Mun <tahermun@gmail.com>
2020-05-21 10:28:20 -0400

    vcf->bwt+marker array

--- vcf_to_bwt.py
+++ vcf_to_bwt.py
@@ -5,8 +5,7 @@ import multiprocessing as mp
 import os
 import logging
 
-PARSE_EXTS  = ['bwlast', 'bwsai', 'dict', 'docs', 'fa', 'ilist', 'log', 'mai',
-               'n', 'occ', 'parse']
+PARSE_EXTS  = ['bwlast', 'bwsai', 'dict', 'docs', 'fa', 'ilist', 'log', 'mai', 'n', 'occ', 'parse']
 
 def clean_parse_files(prefix):
     for ext in PARSE_EXTS:
@@ -14,6 +13,7 @@ def clean_parse_files(prefix):
             os.remove("{}.{}".format(prefix, ext))
         except FileNotFoundError:
             pass
+
 
 class VcfToXArgs:
 
@@ -29,6 +29,7 @@ class VcfToXArgs:
         self.save_fasta = other.save_fasta
         self.ref_only = False
         self.m = other.marker_array
+
 
 class VcfScanCmd:
     def __init__(self, args):
@@ -60,6 +61,7 @@ class VcfScanCmd:
             cmd += ['-m']
         cmd += self.vcfs
         return cmd
+
 
 def vcf_to_fa(args, ref=False):
     cmd_builder = VcfScanCmd(args)
@@ -103,11 +105,13 @@ def vcf_to_parse(args, ref=False):
         vcf_scan_proc.wait()
     log.close()
 
-class vcf_to_fa_builder(ref=False):
+
+class vcf_to_fa_builder:
     def __init__(self, ref=False):
         self.ref = ref
     def __call__(self, args):
         return vcf_to_fa(args, self.ref)
+
 
 class vcf_to_parse_builder:
     def __init__(self, ref=False):
@@ -115,29 +119,20 @@ class vcf_to_parse_builder:
     def __call__(self, args):
         return vcf_to_parse(args, self.ref)
 
-def vcf_to_bwt_no_merge(args):
-    samples = open(args.samples).read().strip().split('\n')
-    thread_args = [VcfToXArgs(i, s, h, args) for i, s in enumerate(samples) for h in ['0', '1']]
-    all_prefixes = [args.o + ".ref"] + [a.full_prefix for a in thread_args]
-    # parsing
-    print("generating fastas")
-    fasta_pool = mp.Pool(processes=args.threads)
-    ref_worker = fasta_pool.apply_async(vcf_to_fa_builder(True), (VcfToXArgs(0, "", 0, args),))
-    fasta_pool.imap_unordered(vcf_to_fa_builder(False), thread_args)
-    ref_worker.get()
-    fasta_pool.close()
-    fasta_pool.join()
-    print("running pfbwt")
-    log = open(args.o + ".log", 'w')
-    cat_cmd = ['cat'] + [p + ".fa" for p in all_prefixes]
-    cat_proc = sp.Popen(cat_cmd, stdout=sp.PIPE)
-    all_pfbwt_cmd = ['./pfbwt-f64', '-s', '--print-docs', '-o', args.o]
-    print(' '.join(cat_cmd) + " | " + ' '.join(all_pfbwt_cmd))
-    sp.run(all_pfbwt_cmd, check=True, stdin=cat_proc.stdout, stderr=log)
-    cat_proc.wait()
+
+def merge_marker_indexes(args, thread_args, logger, log_fp):
+    logger.info("merging marker indexes")
+    length = int(open(args.o + ".ref.n").read().strip())
+    cmd = ['./merge_marker_indexes', str(length), args.o + ".mai"] + [args.o + ".ref.mai"] + [a.full_prefix + ".mai" for a in thread_args]
+    logger.info(" ".join(cmd))
+    sp.run(cmd, check=True, stdout=log_fp, stderr=sp.PIPE)
+    logger.info("merged marker indexes")
 
 
-def vcf_to_bwt_w_merge(args):
+def vcf_to_bwt(args):
+    if not args.samples:
+        sys.stderr.write("no support yet for specification of zero samples\n")
+        exit(1)
     samples = open(args.samples).read().strip().split('\n')
     thread_args = [VcfToXArgs(i, s, h, args) for i, s in enumerate(samples) for h in ['0', '1']]
     all_prefixes = [args.o + ".ref"] + [a.full_prefix for a in thread_args]
@@ -151,45 +146,70 @@ def vcf_to_bwt_w_merge(args):
     stderr_h.setFormatter(logging.Formatter('[%(asctime)s] %(message)s') )
     logger.addHandler(log_h)
     logger.addHandler(stderr_h)
-
     # parsing
-    logger.info("generating parses from VCF")
-    parse_pool = mp.Pool(processes=args.threads)
-    ref_proc = parse_pool.apply_async(vcf_to_parse_builder(True), (VcfToXArgs(0, "", 0, args),))
-    parse_pool.imap_unordered(vcf_to_parse_builder(False), thread_args)
-    ref_proc.get()
-    parse_pool.close()
-    parse_pool.join()
-    logger.info("done generating parses from VCF")
-    # merge
-    logger.info("merging parses")
-    merge_pfp_cmd = ['./merge_pfp', '-s', '--parse-bwt', '--docs', '-o', args.o, '-t', str(args.threads)] + all_prefixes
-    logger.info(" ".join(merge_pfp_cmd))
-    sp.run(merge_pfp_cmd, stdout=log_fp, stderr=sp.PIPE, check=True)
-    logger.info("done merging parses")
-    if args.clean:
-        logger.info("cleaning files")
-        for prefix in all_prefixes:
-            clean_parse_files(prefix)
+    if args.no_merge:
+        logger.info("generating fasta files from VCF")
+        fasta_pool = mp.Pool(processes=args.threads)
+        ref_proc = fasta_pool.apply_async(vcf_to_fa_builder(True), (VcfToXArgs(0, "", 0, args),))
+        fasta_pool.imap_unordered(vcf_to_fa_builder(False), thread_args)
+        ref_proc.get()
+        fasta_pool.close()
+        fasta_pool.join()
+        logger.info("done generating fastas from VCF")
+        # merging
+        if args.marker_array:
+            merge_marker_indexes(args, thread_args, logger, log_fp)
+        logger.info("generating parse (directly from fasta files)")
+        parse_cmd = ['./pfbwt-f64', '--parse-only', '-s', '--print-docs', '-o', args.o]
+        cat_cmd = ['cat'] + [args.o + ".ref.fa"] + [a.full_prefix + ".fa" for a in thread_args]
+        cat_proc = sp.Popen(cat_cmd, stdout=sp.PIPE, stderr=log_fp)
+        logger.info(" ".join(cat_cmd) + " | " + " ".join(parse_cmd))
+        parse_proc = sp.run(parse_cmd, stdin=cat_proc.stdout, stderr=sp.STDOUT, stdout=log_fp)
+        cat_proc.wait()
+        logger.info("generated parse")
+    else:
+        logger.info("generating parses from VCF")
+        parse_pool = mp.Pool(processes=args.threads)
+        ref_proc = parse_pool.apply_async(vcf_to_parse_builder(True), (VcfToXArgs(0, "", 0, args),))
+        parse_pool.imap_unordered(vcf_to_parse_builder(False), thread_args)
+        ref_proc.get()
+        parse_pool.close()
+        parse_pool.join()
+        logger.info("done generating parses from VCF")
+        # merging
+        if args.marker_array:
+            merge_marker_indexes(args, thread_args, logger, log_fp)
+        logger.info("merging parses")
+        merge_pfp_cmd = ['./merge_pfp', '-s', '--parse-bwt', '--docs', '-o', args.o, '-t', str(args.threads)] + all_prefixes
+        logger.info(" ".join(merge_pfp_cmd))
+        sp.run(merge_pfp_cmd, stdout=log_fp, stderr=sp.PIPE, check=True)
+        logger.info("done merging parses")
+        if args.clean:
+            logger.info("cleaning files")
+            for prefix in all_prefixes:
+                clean_parse_files(prefix)
     # construct BWT
     logger.info("constructing BWT")
-    pfbwt_cmd = ['./pfbwt-f64', '-s', '--pfbwt-only', '--print-docs', '-o', args.o]
+    pfbwt_cmd = ['./pfbwt-f64', '--stdout', 'sa', '-s', '--pfbwt-only', '--print-docs', '-o', args.o]
     logger.info(" ".join(pfbwt_cmd))
-    sp.run(pfbwt_cmd, stdout=log_fp, stderr=sp.PIPE, check=True)
+    pfbwt_proc = sp.Popen(pfbwt_cmd, stdout=sp.PIPE, stderr=log_fp)
+    if args.marker_array:
+        logger.info("also constructing marker array from marker index")
+        marker_array_cmd = ['./marker_index_to_array', args.o + ".mai", '-', args.o + ".ma"]
+        logger.info(" ".join(marker_array_cmd))
+        marker_array_proc = sp.run(marker_array_cmd, stdin=pfbwt_proc.stdout, stderr=sp.STDOUT, stdout=log_fp, check=True)
+        pfbwt_proc.wait()
+    else:
+        sa_fp = open(args.o + ".sa", "wb")
+        sa_save_proc = sp.run(['cat'], stdin=pfbwt_proc.stdout, stdout=sa_fp, stderr=log_fp, check=True)
+        pfbwt_proc.wait()
+        sa_fp.close()
     logger.info("done constructing BWT")
-
+    if args.clean and not args.keep_parse:
+        logger.info("cleaning final parse files")
+        clean_parse_files(args.o)
     log_h.close()
     log_fp.close()
-
-
-def vcf_to_bwt(args):
-    if not args.samples:
-        sys.stderr.write("no support yet for specification of zero samples\n")
-        exit(1)
-    if args.no_merge:
-        vcf_to_bwt_no_merge(args)
-    else:
-        vcf_to_bwt_w_merge(args)
 
 
 if __name__ == "__main__":
@@ -203,6 +223,7 @@ if __name__ == "__main__":
     parser.add_argument("--no_merge", action='store_true', help="generate a BWT from a non-merged parse of text collection")
     parser.add_argument("--clean", action='store_true', help="cleanup intermediate files as we go")
     parser.add_argument("--marker_array", "-m", action='store_true', help="cleanup intermediate files as we go")
+    parser.add_argument("--keep_parse", action='store_true', help="keeps the final parse (for when --clean is used)")
     args = parser.parse_args()
 
     vcf_to_bwt(args)
